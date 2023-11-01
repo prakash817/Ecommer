32c3f12d59596fbf5f0e0dc514426ab96fa24248
prakash gupta <prakash.gupta@masaischool.com>
2023-11-02 01:00:23 +0530

    new letter done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Category from "../components/Category";
 import Product from "../components/Product";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Category />
       <Product />
+      <NewsLetter />
     </div>
   );
 };
