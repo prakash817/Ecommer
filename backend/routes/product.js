9988d5361566937f6665574dd28ee9b670771af3
prakash gupta <prakash.gupta@masaischool.com>
2023-11-12 00:19:01 +0530

    product route  completed

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,3 +1,83 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+const { verifyTokenAndAdmin } = require("./verifyToken");
+
+// CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//UPDATED PRODUCT
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//delete
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    console.log(product, "chekc");
+    res
+      .status(200)
+      .json(`Product Name (${product.title}) has been successfully deleted`);
+  } catch (error) {
+    res.status(200).json(error);
+  }
+});
+
+//Get Perticular Product
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(200).json(error);
+  }
+});
+
+//Get All Product (anybody can see)
+
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  console.log(qCategory);
+  try {
+    let products;
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: { $in: [qCategory] },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(200).json(error);
+  }
+});
 
 module.exports = router;
